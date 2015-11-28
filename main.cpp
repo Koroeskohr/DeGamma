612ce856ec2a52593101daac6110578c7d30888f
Victor Viale <somebodywas@gmail.com>
2015-11-28 13:58:50 +0100

    Done with cmake
    Hopefully

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+#include <common.hpp>
+
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <assimp/DefaultLogger.hpp>
+
 int main(){
     std::cout << "hello" << std::endl;
+    Assimp::Importer importer;
+    std::string file = "airboat.obj";
+    const aiScene *scene = importer.ReadFile(file, aiProcessPreset_TargetRealtime_Fast);//aiProcessPreset_TargetRealtime_Fast has the configs you'll need
+
+    if (!scene)
+    {
+        std::cout << "Error loading file: (assimp:) " << importer.GetErrorString();
+        return false;
+    }
+    aiMesh *mesh = scene->mMeshes[0]; //assuming you only want the first mesh
+
+    std::cout << scene->mNumMeshes << std::endl;
+
+
 }
