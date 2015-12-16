ad58acba17193d3fecc7e54c99348642d10497e7
Victor Viale <somebodywas@gmail.com>
2015-12-16 14:41:15 +0100

    Add transform struct

---
+++ glimac/include/Transform.hpp
@@ -0,0 +1,31 @@
+//
+// Created by koro on 16/12/15.
+//
+
+#ifndef DEGAMMA_TRANSFORM_HPP
+#define DEGAMMA_TRANSFORM_HPP
+
+#include <common.hpp>
+
+using namespace glm;
+
+struct Transform {
+    mat4 modelMatrix;
+
+    mat4 rotation;
+    vec3 position;
+    vec3 scale;
+
+    void updateModelMatrix(){
+        modelMatrix = mat4(0);
+        modelMatrix *= rotation;
+        modelMatrix = glm::translate(modelMatrix, position);
+        modelMatrix = glm::scale(modelMatrix, scale);
+    }
+
+    Transform()
+        : rotation(0), position(0), scale(1)
+    { }
+};
+
+#endif //DEGAMMA_TRANSFORM_HPP
