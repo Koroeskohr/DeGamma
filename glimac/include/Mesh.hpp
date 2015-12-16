e70bcae718d8382b2f9d97ba9aa34edd8f330257
Victor Viale <somebodywas@gmail.com>
2015-12-16 22:54:26 +0100

    Mesh references a texture by its name

--- glimac/include/Mesh.hpp
+++ glimac/include/Mesh.hpp
@@ -6,6 +6,9 @@
 #define DEGAMMA_MESH_HPP
 
 #include <assimp/scene.h>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+
 
 #include <vector>
 #include "Vertex.hpp"
@@ -15,13 +18,17 @@ namespace glimac {
 
 class Mesh {
 public:
-    Mesh(const aiMesh* mesh); // TODO : , const aiMaterial *material = nullptr ??
+    Mesh(const aiMesh* mesh, const std::string& materialName);
+
+
 
 
 private:
     unsigned int mVerticesAmount;
     std::vector<Vertex> mVertices;
     std::vector<uint32_t> mIndices;
+
+    std::string mMaterialName;
 
     //TODO : do we add material to the mesh ?
     // probably not
--- glimac/src/Mesh.cpp
+++ glimac/src/Mesh.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Mesh.hpp"
+
+namespace glimac {
+    Mesh::Mesh (const aiMesh *mesh, const std::string& materialName)
+        : mMaterialName(materialName)
+    {
+        /*
+         * TODO : get aiMesh from scene in Model class
+         * /
+    }
+
+}
+
