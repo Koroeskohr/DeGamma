bde59786a2b7fbf474efea60755bc32852043797
Victor Viale <somebodywas@gmail.com>
2015-12-16 22:54:53 +0100

    Example model with an obj we have

---
+++ app/include/models/AirboatModel.hpp
@@ -0,0 +1,25 @@
+//
+// Created by koro on 16/12/15.
+//
+
+#ifndef DEGAMMA_AIRBOATMODEL_HPP
+#define DEGAMMA_AIRBOATMODEL_HPP
+
+#include "Model.hpp"
+#include "Mesh.hpp"
+#include "Texture.hpp"
+#include "ResourceManager.hpp"
+
+using namespace glimac;
+
+class AirboatModel: public Model {
+
+public:
+    AirboatModel("airboat.obj") //FIXME
+    {
+
+
+    }
+};
+
+#endif //DEGAMMA_AIRBOATMODEL_HPP
