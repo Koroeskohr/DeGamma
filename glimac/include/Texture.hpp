406ccddddb5bd72ab858fa1a4c617a9858548640
Victor Viale <somebodywas@gmail.com>
2015-12-16 14:40:54 +0100

    Add basic texture class

---
+++ glimac/include/Texture.hpp
@@ -0,0 +1,14 @@
+//
+// Created by koro on 16/12/15.
+//
+
+#ifndef DEGAMMA_TEXTURE_HPP
+#define DEGAMMA_TEXTURE_HPP
+
+
+class Texture {
+    //TODO
+};
+
+
+#endif //DEGAMMA_TEXTURE_HPP
---
+++ glimac/src/Texture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by koro on 16/12/15.
+//
+
+#include "Texture.hpp"
