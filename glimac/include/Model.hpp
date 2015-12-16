418efb877854e906c904bf363c81b5a03f0d06e5
Victor Viale <somebodywas@gmail.com>
2015-12-16 22:52:36 +0100

    Change Model representation of collections
    Textures became map because a mesh needs to reference a material (a
    texture)

--- glimac/include/Model.hpp
+++ glimac/include/Model.hpp
@@ -5,18 +5,24 @@
 #ifndef DEGAMMA_MODEL_HPP
 #define DEGAMMA_MODEL_HPP
 
+#include <map>
+
 #include "Mesh.hpp"
 #include "Texture.hpp"
+#include "ResourceManager.hpp"
+
 
 namespace glimac {
 
 class Model {
-    Mesh mMesh;
-    Texture mTexture;
-
 public:
-    virtual Model() = 0;
+    Model(const std::string& fileName);
     virtual ~Model() = 0;
+
+
+private:
+    std::vector<Mesh> mMeshes;
+    std::map<std::string, Texture> mTextures;
 };
 
 }
--- glimac/src/Model.cpp
+++ glimac/src/Model.cpp
@@ -3,3 +3,22 @@
 //
 
 #include "Model.hpp"
+
+namespace glimac {
+    Model::Model (const std::string& fileName)
+    {
+        const aiScene* scene = ResourceManager::import("airboat.obj");
+        if(scene->mNumMeshes == 0)
+            throw std::runtime_error("Scene was empty");
+
+        //TODO vectorify c tables and call Model constructor
+        /*
+        for(int i = 0; i < scene->mNumMeshes; i++)
+            mMeshes.push_back(Mesh(scene->mMeshes[i]));
+
+        scene->mMeshes[i],
+        scene->mMaterials[aScene->mMeshes[i]->mMaterialIndex
+
+         */
+    }
+}
