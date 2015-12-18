3cbcc2fa30804d40ebf8686a875ca7ea26950bad
Victor Viale <somebodywas@gmail.com>
2015-12-18 21:26:08 +0100

    Revert "Remove useless stuff, add glEnable... global defines"
    This reverts commit 85ba70e8e714f596aa909fbc7c251d133981be26.

--- glimac/include/common.hpp
+++ glimac/include/common.hpp
@@ -4,6 +4,12 @@
 #include "glm.hpp"
 #include "FileLogger.hpp"
 
-#define VERTEX_ATTR_POSITION 0
-#define VERTEX_ATTR_NORMAL 1
-#define VERTEX_ATTR_TEX_COORDS 2
+namespace glimac {
+
+struct ShapeVertex {
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec2 texCoords;
+};
+
+}
