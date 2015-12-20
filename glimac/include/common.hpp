e0a17fe2337b4e1e117f04f9703fb1d6b2d1af98
Victor Viale <somebodywas@gmail.com>
2015-12-20 19:12:32 +0100

    Adds vertex attribute indices defines

--- glimac/include/common.hpp
+++ glimac/include/common.hpp
@@ -4,6 +4,11 @@
 #include "glm.hpp"
 #include "FileLogger.hpp"
 
+#define VERTEX_ATTR_POSITION 0
+#define VERTEX_ATTR_NORMAL 1
+#define VERTEX_ATTR_TEX_COORDS 2
+
+
 namespace glimac {
 
 struct ShapeVertex {
