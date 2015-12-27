0af6c82a5cecabd79ed1710b52b7f6493b66e500
Victor Viale <somebodywas@gmail.com>
2015-12-27 22:28:55 +0100

    Add scene logic

--- glimac/src/Scene.cpp
+++ glimac/src/Scene.cpp
@@ -10,7 +10,44 @@ namespace glimac{
         loadPrograms();
     }
 
-    void Scene::loadPrograms () {
-
+    Scene::~Scene () {
+        for(auto renderable: mRenderables){
+            delete renderable;
+        }
+        for(auto program: mPrograms){
+            delete program;
+        }
     }
+
+    void Scene::update () {
+        for(auto renderable: mRenderables){
+            renderable->update();
+        }
+    }
+
+    void Scene::render () {
+        for(auto renderable: mRenderables){
+            renderable->render();
+        }
+    }
+
+    void Scene::addRenderable (Renderable *renderable) {
+        mRenderables.push_back(renderable);
+    }
+
+    void Scene::loadPrograms () {
+        //TODO : more shaders ?
+        Program * p  = glimac::loadProgram("shaders/3D.vs.glsl", "shaders/3D.fs.glsl");
+        addProgram(p);
+        setProgram(p);
+    }
+
+    void Scene::addProgram (Program * program) {
+        mPrograms.push_back(program);
+    }
+
+    void Scene::setProgram (Program * program) {
+        mCurrentProgram = program;
+    }
+
 }
