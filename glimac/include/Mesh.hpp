937aeb3a04e34617434ca140d2e5817769fd3324
Victor Viale <somebodywas@gmail.com>
2015-12-29 23:12:20 +0100

    forever alone, and already forgotten

--- glimac/include/Mesh.hpp
+++ glimac/include/Mesh.hpp
@@ -29,7 +29,6 @@ public:
     std::vector<unsigned> mIndices;
     unsigned long mVerticesAmount;
     std::string mMaterialName;
-    glm::vec3 mdiffColor;
 
     /*std::vector<Vertex> * getVertices() const;
     std::vector<unsigned> * getIndices() const;
