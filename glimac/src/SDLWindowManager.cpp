c201322baccdff0a50787756796abb54201878a1
Victor Viale <somebodywas@gmail.com>
2015-12-30 16:48:41 +0100

    Add as comment potential camera solutions

--- glimac/src/SDLWindowManager.cpp
+++ glimac/src/SDLWindowManager.cpp
@@ -43,6 +43,10 @@ SDLWindowManager::SDLWindowManager(uint32_t width, uint32_t height, const char* 
         return;
     }
     SDL_WM_SetCaption(title, nullptr);
+
+    //SDL_ShowCursor(SDL_DISABLE);
+    //SDL_WM_GrabInput(SDL_GRAB_ON);
+
 #endif
 }
 
